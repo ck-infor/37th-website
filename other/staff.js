b95fdc1ff8ce9720b2c33fd204361dc839322e71
Franz <franz.e.lee@gmail.com>
2024-08-08 00:23:25 +0800

    Add mobile mode.

--- other/staff.js
+++ other/staff.js
@@ -92,3 +92,13 @@ function check() {
 //         });
 //     });
 // });
+
+function menu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } 
+    else {
+        x.style.display = "block";
+    }
+}
