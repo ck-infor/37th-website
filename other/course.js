767e0770f7d29a551837c7d986d99f92c4910e9a
Franz <franz.e.lee@gmail.com>
2024-08-08 00:24:57 +0800

    Add mobile mode.

--- other/course.js
+++ other/course.js
@@ -185,3 +185,13 @@ function newMessageBox(id) {
         top : "auto"
     }) 
 }
+
+function menu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } 
+    else {
+        x.style.display = "block";
+    }
+}
