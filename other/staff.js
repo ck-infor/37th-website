7b278dc2babace295019dc9b3b4692c3027728c0
007 <leow7953@gmail.com>
2024-08-18 11:17:36 +0800

    add a list of position in staff.js

--- other/staff.js
+++ other/staff.js
@@ -1,10 +1,12 @@
 function start() {
     var nameList = [
-        "社長&學術 林顯至", "副社長&網管 李則聿", "外交長 林俐辰", "學術長 翁浩洋",
-        "學術 温世揚", "行政長 陳秉澤", "行政&衛生 孫唯善", "總務 林楚鈞", 
-        "美宣 李宸漢", "美宣 王丞佑"
+        "林顯至", "李則聿", "林俐辰", "翁浩洋",
+        "温世揚", "陳秉澤", "孫唯善", "林楚鈞", 
+        "李宸漢", "王丞佑"
     ];
-
+    var position= [
+        "社長 X 學術", "副社長 X 網管","外交長", "學術長", "學術", "行政長", "行政&衛生", "總務", "美宣", "美宣"
+    ];
     var descriptionList = [
         "<h2>#</h2><p></p><h2>#</h2><p></p><h2>#</h2><p></p><h2>#</h2><p></p>",
         "<h2>#</h2><p></p><h2>#</h2><p></p><h2>#</h2><p></p><h2>#</h2><p></p>",
@@ -28,7 +30,6 @@ function start() {
         span.innerHTML = nameList[i];
         span.classList.add("highLight");
         span.classList.add("hidden");
-        span.style.backgroundColor = "gold";
         element.appendChild(span);
         target.appendChild(element);
 
