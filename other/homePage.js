f2d19853aa5c5e38d8cf32b08cc6f55bf3c91785
007 <leow7953@gmail.com>
2024-08-20 11:06:45 +0800

    add information of izcc to the main page

--- other/homePage.js
+++ other/homePage.js
@@ -62,6 +62,9 @@ function menu() {
 
 function start() {
     var topicList = ["建國中學資訊社", "IZCC"];
+    var clubchnameList=["建國中學資訊社", "中山女高資訊研究社", "成功高中電子計算機研習社", "景美女中資訊研究社"]
+    var clubennameList = ["INFOR", "ZSISC", "CKCSC", "CMIOC"]
+    var clublogoList =["./other/photos/clublogo/infor.png", "./other/photos/clublogo/zsisc.png", "./other/photos/clublogo/ckcsc.png", "./other/photos/clublogo/cmioc.png"]
     var introduceTextList = ["建中資訊社是一個專注於電腦和資訊科技的學術性社團。在這裡，你可以學習程式設計、演算法、網頁開發、人工智慧等科技領域的課程。除此之外，你還可以認識許多學術力一流的同儕，並與友校的朋友交流。",
     "IZCC是建中資訊 (INFOR)、 中山資研 (ZSISC)、成功電研 (CKCSC)與景美電資 (CMIOC) 的合稱。IZCC由一群熱愛資訊的人們組成。放學會舉辦課程一同學習，也會共同舉辦活動，像是暑訓、秋遊、耶誕晚會等······。"];
     var paragraph = document.getElementById("introduction");
@@ -74,8 +77,6 @@ function start() {
         var topic = document.createElement("div");
         topic.innerHTML = topicList[i];
         topic.classList.add("topic");
-        var hrLine = document.createElement("hr");
-        topic.appendChild(hrLine);
         container.appendChild(topic);
 
         var introText = document.createElement("div");
@@ -83,7 +84,31 @@ function start() {
         introText.classList.add("introText");
         container.appendChild(introText);
 
-        paragraph.appendChild(container);
+        var ul=document.createElement("ul");
+        for(var i=0;i<clublogoList.length; i++){
+            var li=document.createElement("li");
+            var photo = new Image();
+            photo.src = clublogoList[i];
+            photo.alt = clublogoList[i] + "照片";
+            photo.classList.add("clublogo");
+            container.appendChild(photo);
+            li.appendChild(photo);
+
+            var chname=document.createElement("div");
+            chname.classList.add("clubchname");
+            chname.innerHTML = clubchnameList[i];
+            container.appendChild(chname);
+            li.appendChild(chname);
+
+            var enname=document.createElement("div");
+            enname.classList.add("clubenname");
+            enname.innerHTML = clubennameList[i];
+            container.appendChild(enname);          
+            li.appendChild(enname);
+            ul.appendChild(li);
+            }
+        container.appendChild(ul);
+        paragraph.appendChild(container);        
     }
     setInterval(check(), 50);
 }
