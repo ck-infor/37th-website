7afa1307317795d99383c412a867dfbddecc7c34
Franz <franz.e.lee@gmail.com>
2024-12-11 22:33:49 +0800

    Update announcement.

--- other/homePage.js
+++ other/homePage.js
@@ -134,7 +134,7 @@ function start() {
                 }
             },
             title : "最新公告",
-            message : "<p class=\"msgText\">我們調換了星期二、四放課的順序。(Sep. 16, 2024)</p>",
+            message : "<p class=\"msgText\">我們更新了活動照片與資訊。(Dec. 11, 2024)</p>",
             input : {
                 checkbox : {
                     type : "checkbox",
